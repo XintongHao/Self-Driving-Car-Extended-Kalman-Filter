c22c15c9a023bdcba83bddb7395038217ef34aae
XintongHao <hxtong@bu.edu>
2019-02-03 21:24:01 +0000

    tool

--- src/tools.cpp
+++ src/tools.cpp
@@ -14,6 +14,23 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+  VectorXd rmse(4);
+  rmse <<0, 0, 0, 0;
+  /** check the validity of the following inputs:
+    1. the estimation vector size should not be zero;
+    2. the estimation vector size should equal ground truck vector size;
+  */
+  if(estimations.size() != ground_truth.size() || estimations.size() == 0){
+    cout << "Invalid estimation or ground_truth data" << endl;
+    return rmse;
+  }
+  
+  // accumulate squared residuals
+  for( unsigned int i = 0; i < estimations.size(); i++){
+    VectorXd residual = 
+  }
+
+    
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -21,4 +38,5 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    * TODO:
    * Calculate a Jacobian here.
    */
+  
 }
